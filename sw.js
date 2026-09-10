5a4709c4f9bb16bd8e4d4ce3d5e8ff800d395f12
rmambulancier-pixel <rm.ambulancier@gmail.com>
2026-09-10 21:49:16 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'mesheures-shell-v16.0';
-const SHELL = ['./', './index.html', './manifest.json', './icon.svg', './style/refonte.css?v=16.0', './scripts/app-core.js', './scripts/app-pwa.js', './scripts/app-ui.js', './scripts/app-parser.js', './scripts/app-plugins.js', './scripts/app.js'];
+const CACHE = 'mesheures-shell-v16.0.1';
+const SHELL = ['./', './index.html', './manifest.json', './icon.svg', './style/refonte.css?v=16.0.1', './scripts/app-core.js', './scripts/app-pwa.js', './scripts/app-ui.js', './scripts/app-parser.js', './scripts/app-plugins.js', './scripts/app.js'];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(SHELL)).catch(() => {}).then(() => self.skipWaiting()));
